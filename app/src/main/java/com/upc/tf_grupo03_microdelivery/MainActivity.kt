a175ebad190b540a94b9f06358721cad15e3c825
mespinoza <infinitevx@live.com>
2023-12-02 14:25:26 -0500

    logueo

--- app/src/main/java/com/upc/tf_grupo03_microdelivery/MainActivity.kt
+++ app/src/main/java/com/upc/tf_grupo03_microdelivery/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.upc.tf_grupo03_microdelivery
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btncrearusuario: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+/*
+    private fun crearUsuario(){
+        btncrearusuario.setOnClickListener({
+            val intent = Intent(this,)
+
+        })
+    }
+    */
+
 }
---
+++ app/src/main/java/com/upc/tf_grupo03_microdelivery/entidades/Usuarios.kt
@@ -0,0 +1,15 @@
+package com.upc.tf_grupo03_microdelivery.entidades
+
+class Usuarios {
+
+    var id:Int = 0
+    lateinit var dni:String
+    lateinit var nombres:String
+    lateinit var apellidos:String
+    lateinit var usuario:String
+    lateinit var contrasena:String
+    lateinit var correo:String
+    lateinit var distrito:String
+    lateinit var direccion:String
+    var tipoUsuario:Int = 0
+}
