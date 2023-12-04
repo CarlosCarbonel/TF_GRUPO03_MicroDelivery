8909be69a52733a995bf4aa75fe15d0d64baca4c
CarlosCarbonel <150833070+CarlosCarbonel@users.noreply.github.com>
2023-12-04 02:55:45 -0500

    Lista de usuarios creados

--- app/src/main/java/com/upc/tf_grupo03_microdelivery/UsuarioActivity.kt
+++ app/src/main/java/com/upc/tf_grupo03_microdelivery/UsuarioActivity.kt
@@ -1,17 +1,22 @@
 package com.upc.tf_grupo03_microdelivery
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import com.upc.tf_grupo03_microdelivery.util.ListaRepartidor
 
 class UsuarioActivity : AppCompatActivity() {
 
     private lateinit var txtmsnombapellidos: TextView
+    private lateinit var btnbuscarepartidor: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_usuario)
         verificarAcceso()
+        buscarRepartidor()
     }
 
     private fun verificarAcceso() {
@@ -19,5 +24,13 @@ class UsuarioActivity : AppCompatActivity() {
             txtmsnombapellidos = findViewById(R.id.txtmsnombapellidos)
             txtmsnombapellidos.setText(intent.getStringExtra("p_name"))
         }
+    }
+
+    private fun buscarRepartidor(){
+        btnbuscarepartidor = findViewById(R.id.btnbuscarepartidor)
+        btnbuscarepartidor.setOnClickListener({
+            val intent = Intent(this, ListaRepartidor::class.java)
+            startActivity(intent)
+        })
     }
 }
