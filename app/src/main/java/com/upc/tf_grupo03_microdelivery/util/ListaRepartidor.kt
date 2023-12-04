85c875be2737ba0d1f4e6c9eba7b7ff65d848d09
PC <andres_puelles@hotmail.com>
2023-12-03 21:31:02 -0500

    Se agregó el diseño de cada fila de la lista  y se agrego otro empty view activity para el recycler

---
+++ app/src/main/java/com/upc/tf_grupo03_microdelivery/util/ListaRepartidor.kt
@@ -0,0 +1,12 @@
+package com.upc.tf_grupo03_microdelivery.util
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.upc.tf_grupo03_microdelivery.R
+
+class ListaRepartidor : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_repartidor)
+    }
+}
