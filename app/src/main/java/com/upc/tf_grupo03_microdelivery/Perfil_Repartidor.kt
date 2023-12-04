20410ec6f2109679f4723017eca9e4377e6f2c07
PC <andres_puelles@hotmail.com>
2023-12-03 22:13:55 -0500

    Se añadio el perfil de repartidor

---
+++ app/src/main/java/com/upc/tf_grupo03_microdelivery/Perfil_Repartidor.kt
@@ -0,0 +1,11 @@
+package com.upc.tf_grupo03_microdelivery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Perfil_Repartidor : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perfil_repartidor)
+    }
+}
