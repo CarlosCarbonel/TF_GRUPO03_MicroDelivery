a7b6f4ef6a8a3568f27d53b8021bed82f801bc20
CarlosCarbonel <150833070+CarlosCarbonel@users.noreply.github.com>
2023-12-05 22:31:24 -0500

    Lista de usuarios creados Agregar usuarios predefinidos en BD Utilización de cada fila de repartidor como boton para ir a su hoja detalle Ingreso a pantalla detalle de repartidor Boton con popup para simular llamada + popup basico listo Tabla y gestión de contactos + interface Contacto

--- app/src/main/java/com/upc/tf_grupo03_microdelivery/UsuarioActivity.kt
+++ app/src/main/java/com/upc/tf_grupo03_microdelivery/UsuarioActivity.kt
@@ -6,17 +6,20 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import com.upc.tf_grupo03_microdelivery.util.ListaRepartidor
+import com.upc.tf_grupo03_microdelivery.util.lista_contactos
 
 class UsuarioActivity : AppCompatActivity() {
 
     private lateinit var txtmsnombapellidos: TextView
     private lateinit var btnbuscarepartidor: Button
+    private lateinit var btncontactos: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_usuario)
         verificarAcceso()
         buscarRepartidor()
+        buscarContacto()
     }
 
     private fun verificarAcceso() {
@@ -30,6 +33,14 @@ class UsuarioActivity : AppCompatActivity() {
         btnbuscarepartidor = findViewById(R.id.btnbuscarepartidor)
         btnbuscarepartidor.setOnClickListener({
             val intent = Intent(this, ListaRepartidor::class.java)
+            startActivity(intent)
+        })
+    }
+
+    private fun buscarContacto(){
+        btncontactos = findViewById(R.id.btncontactos)
+        btncontactos.setOnClickListener({
+            val intent = Intent(this, lista_contactos::class.java)
             startActivity(intent)
         })
     }
--- app/src/main/java/com/upc/tf_grupo03_microdelivery/util/Lista_contactos.kt
+++ app/src/main/java/com/upc/tf_grupo03_microdelivery/util/Lista_contactos.kt
@@ -1,7 +1,8 @@
-package com.upc.tf_grupo03_microdelivery
+package com.upc.tf_grupo03_microdelivery.util
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.upc.tf_grupo03_microdelivery.R
 
 class lista_contactos : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
