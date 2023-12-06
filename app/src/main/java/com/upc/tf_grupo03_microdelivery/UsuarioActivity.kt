47d09f8a56dd07e786604eb24ca555c806b7a34a
mespinoza <infinitevx@live.com>
2023-12-05 23:44:33 -0500

    seteando datos del usuario

--- app/src/main/java/com/upc/tf_grupo03_microdelivery/UsuarioActivity.kt
+++ app/src/main/java/com/upc/tf_grupo03_microdelivery/UsuarioActivity.kt
@@ -14,6 +14,7 @@ class UsuarioActivity : AppCompatActivity() {
     private lateinit var txtmsnombapellidos: TextView
     private lateinit var btnbuscarepartidor: Button
     private lateinit var btncontactos: Button
+    private lateinit var txtmscorreo: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +27,9 @@ class UsuarioActivity : AppCompatActivity() {
     private fun verificarAcceso() {
         if (intent.hasExtra("p_name")) {
             txtmsnombapellidos = findViewById(R.id.txtmsnombapellidos)
+            txtmscorreo = findViewById(R.id.txtmscorreo)
             txtmsnombapellidos.setText(intent.getStringExtra("p_name"))
+            txtmscorreo.setText(intent.getStringExtra("p_email"))
         }
     }
 
