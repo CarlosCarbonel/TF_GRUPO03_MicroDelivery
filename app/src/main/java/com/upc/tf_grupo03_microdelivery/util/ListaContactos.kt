e1c138e8790f1a4937b97dc2b4ee6c7fe30ec954
CarlosCarbonel <150833070+CarlosCarbonel@users.noreply.github.com>
2023-12-05 23:25:28 -0500

    Lista de usuarios creados Agregar usuarios predefinidos en BD Utilización de cada fila de repartidor como boton para ir a su hoja detalle Ingreso a pantalla detalle de repartidor Boton con popup para simular llamada + popup basico listo Tabla y gestión de contactos + interface Contacto

--- app/src/main/java/com/upc/tf_grupo03_microdelivery/util/ListaContactos.kt
+++ app/src/main/java/com/upc/tf_grupo03_microdelivery/util/ListaContactos.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.upc.tf_grupo03_microdelivery.R
 
-class lista_contactos : AppCompatActivity() {
+class ListaContactos : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_contactos)
